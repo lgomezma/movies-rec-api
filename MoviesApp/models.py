17b82104403fa1613be5723f3a41f0f2565f05cb
Luis Gomez <lgomezma@gmail.com>
2015-03-26 20:38:13 +0100

    Added Users model

--- MoviesApp/models.py
+++ MoviesApp/models.py
@@ -30,3 +30,11 @@ class WishlistItems(db.Model):
 		self.id = id
 		self.wishlist_id = wishlist_id
 		self.movie_id = movie_id
+
+class Users(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	device_token = db.Column(db.String(255),unique=True)
+
+	def __init__(self, id, device_token):
+		self.id = id
+		self.device_token = device_token
